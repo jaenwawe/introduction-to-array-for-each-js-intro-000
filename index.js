b9958dad32df642ea9e1065824efc260c75ccb0c
Jae Nwawe <nwawe.jae@gmail.com>
2020-03-08 21:08:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function doToElementsInArray(array,callback){
 function changeCompletely(element, indes, array){
   array[index] = Math.floor(Math.random() * 100 + 2).toString()
   +
-  ${array[index]} 
+  ${array[index]} s!!!
   
 }
 var animals = ["dog", "cat", "squirrel"]
