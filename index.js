aafa1eecbbf64f1c0a2a65e6f1dd29353d894473
Jae Nwawe <nwawe.jae@gmail.com>
2020-03-08 21:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ function doToElementsInArray(array,callback){
   array.forEach(callback);
 }
 
-function
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
 
 
 console.log(animals);
